cf0e2d480b9d5cb19c37e94ff7ae6dda80ff4dbc
Sarun Intaralawan <sarunint@sarunint.com>
2024-09-02 00:16:18 +0700

    chore: update flake

--- nixos-configurations/sarunint-workstation-nixos/wireguard.nix
+++ nixos-configurations/sarunint-workstation-nixos/wireguard.nix
@@ -18,5 +18,21 @@
 
       autostart = false;
     };
+    wg1 = {
+      address = [ "10.31.41.7/32" "fd08:1260:6fd1::7/128"];
+      dns = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+      privateKeyFile = "/home/sarunint-nixos/wireguard-keys/private";
+
+      peers = [
+        {
+          publicKey = "gIJ3ZvpSUwKiKOOCRESLbdfchSEBIaDK63Eyq0FcZFU=";
+          allowedIPs = [ "104.224.54.129/32" ];
+          endpoint = "139.162.18.218:23052";
+          persistentKeepalive = 25;
+        }
+      ];
+
+      autostart = false;
+    };
   };
 }
