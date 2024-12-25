4d183372d4d867af716768bc98b27bd9bf6493d8
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 03:51:13 +0700

    feat: enable VirtualBox

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -3,4 +3,6 @@
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "sarunint" ];
 }
