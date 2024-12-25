227c2df7209799bf7a25ca42fe010b9f2c44f756
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 04:53:30 +0700

    feat: add OVMFFull

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.virt-manager.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd ];
+    };
+  };
   users.users.sarunint.extraGroups = [ "libvirtd" ];
 }
