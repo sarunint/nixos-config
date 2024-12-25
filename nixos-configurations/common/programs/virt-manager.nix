aaa5d8c8711b294c41f540a91b78585a41d0473d
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 04:55:27 +0700

    feat: remove cross-compile firmware

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -6,7 +6,7 @@
     enable = true;
     qemu = {
       swtpm.enable = true;
-      ovmf.packages = [ pkgs.OVMFFull.fd pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd ];
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
     };
   };
   users.users.sarunint.extraGroups = [ "libvirtd" ];
