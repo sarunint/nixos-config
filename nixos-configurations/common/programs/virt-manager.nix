38021adbcb4af9bf9683cd597f3172bfc591b750
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 04:17:06 +0700

    feat: add VirtualBox extensions

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -4,4 +4,5 @@
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "sarunint" ];
   boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 }
