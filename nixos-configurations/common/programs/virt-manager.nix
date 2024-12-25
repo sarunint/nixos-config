bdb21afe08f92cc64868a0aed1d349bf4b2ea104
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 04:26:50 +0700

    feat: change back to virt-manager

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -1,8 +1,6 @@
 { ... }:
 
 {
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "sarunint" ];
-  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  programs.virt-manager.enable = true;
+  users.users.sarunint.extraGroups = [ "libvirtd" ];
 }
