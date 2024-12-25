7288ce80d3c6bf6dd922cbe696f4fa727a09607b
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 03:58:33 +0700

    feat: disable virt-manager

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -1,8 +1,6 @@
 { ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "sarunint" ];
 }
