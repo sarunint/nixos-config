9cdc97a98f7270c3ad5c770f80f2947bd4941185
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 05:23:20 +0700

    feat: use full OVMF again

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -6,7 +6,7 @@
     enable = true;
     qemu = {
       swtpm.enable = true;
-      # ovmf.packages = [ pkgs.OVMFFull.fd ];
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
     };
   };
   users.users.sarunint.extraGroups = [ "libvirtd" ];
