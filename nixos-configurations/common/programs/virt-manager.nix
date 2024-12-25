e6b4903b620b0a21f5ebfa8856bda9f920fda2bb
Sarun Intaralawan <sarunint@sarunint.com>
2024-12-26 05:07:24 +0700

    feat: add normal OVMF firmware

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -6,7 +6,7 @@
     enable = true;
     qemu = {
       swtpm.enable = true;
-      ovmf.packages = [ pkgs.OVMFFull.fd ];
+      ovmf.packages = [ pkgs.OVMF.fd pkgs.OVMFFull.fd ];
     };
   };
   users.users.sarunint.extraGroups = [ "libvirtd" ];
