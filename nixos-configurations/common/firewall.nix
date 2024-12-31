415d2d39623c497d18944fce00b6cca1065c1cf5
Sarun Intaralawan <sarunint@sarunint.com>
2025-01-01 05:40:06 +0700

    feat: temporarily disable nftables

--- nixos-configurations/common/firewall.nix
+++ nixos-configurations/common/firewall.nix
@@ -2,7 +2,7 @@
 
 {
   networking.firewall.enable = true;
-  networking.nftables.enable = true;
+  # networking.nftables.enable = true;
   networking.firewall = {
     allowedTCPPorts = [ 4455 ];
   };
