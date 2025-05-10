4a321b876c11646b92628ec0ebcb7bfee49d2b16
Sarun Intaralawan <sarunint@sarunint.com>
2025-05-10 21:48:40 +0700

    feat: test removing Wifi 7 modules

--- nixos-configurations/sarunint-nixos-laptop/hardware-configuration.nix
+++ nixos-configurations/sarunint-nixos-laptop/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "ath12k" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
