3084b57e8968db1f6f570f79705d18c884dc1597
Sarun Intaralawan <sarunint@sarunint.com>
2025-06-26 09:05:43 +0700

    feat: add swirl

--- nixos-configurations/common/r.nix
+++ nixos-configurations/common/r.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
 
+let 
+  rPackages = with pkgs.rPackages; [
+    swirl
+  ];
+  R-with-my-packages = pkgs.rWrapper.override {
+    packages = rPackages;
+  };
+  RStudio-with-my-packages = pkgs.rstudioWrapper.override {
+    packages = rPackages;
+  };
+in 
 {
   environment.systemPackages = [
-    pkgs.rWrapper
-    pkgs.rstudioWrapper
+    R-with-my-packages
+    RStudio-with-my-packages
   ];
 }
