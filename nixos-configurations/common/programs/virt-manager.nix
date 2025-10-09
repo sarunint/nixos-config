6acfd3ce50ed1c3620d0cc052887231badcf3af2
Sarun Intaralawan <sarunint@sarunint.com>
2025-10-09 18:52:36 +0700

    fix: remove ovmf modules

--- nixos-configurations/common/programs/virt-manager.nix
+++ nixos-configurations/common/programs/virt-manager.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.virt-manager.enable = true;
@@ -6,7 +6,6 @@
     enable = true;
     qemu = {
       swtpm.enable = true;
-      ovmf.packages = [ pkgs.OVMFFull.fd ];
     };
   };
   users.users.sarunint.extraGroups = [ "libvirtd" ];
