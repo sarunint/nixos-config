44de2562f8181e9cd326c7c62addacdb750d510a
Sarun Intaralawan <sarunint@sarunint.com>
2025-10-21 17:26:03 +0700

    feat: add WinBoat components

--- nixos-configurations/common/winboat.nix
+++ nixos-configurations/common/winboat.nix
@@ -1,7 +1,10 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
-  environment.systemPackages = [
+  virtualisation.docker.enable = true;
+
+  environment.systemPackages = with pkgs; [
     inputs.winboat.packages.x86_64-linux.winboat
+    freerdp
   ];
 }
