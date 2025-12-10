6ee46eb0c301688d531d0b4d90cd5bf4ddff2be3
Sarun Intaralawan <sarunint@sarunint.com>
2025-12-10 23:42:16 +0700

    feat: update AppImage-based MuseScore Studio to 4.6.4

--- flake.nix
+++ flake.nix
@@ -57,10 +57,11 @@
       };
       musescore-appimage = let 
         pname = "musescore-appimage";
-        version = "4.6.3";
+        version = "4.6.4";
+        buildNumber = "253351238";
         src = pkgs.fetchurl {
-          url = "https://cdn.jsdelivr.net/musescore/v${version}/MuseScore-Studio-${version}.252940956-x86_64.AppImage ";
-          hash = "sha256-YWdQhWgslsYZCkxnlB4JpMNX5CGNUiCIhrJzmtpJuwA=";
+          url = "https://cdn.jsdelivr.net/musescore/v${version}/MuseScore-Studio-${version}.${buildNumber}-x86_64.AppImage";
+          hash = "sha256-BdNktm5ThSG7+8oMBOEZAifPVrsT4HGQYwnsx35hM/0=";
         };
         appimageContents = pkgs.appimageTools.extractType2 { inherit pname version src; };
       in
