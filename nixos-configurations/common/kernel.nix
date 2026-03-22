b62334019af8f38c82e8bf4685387756d99ac2f7
Sarun Intaralawan <sarunint@sarunint.com>
2026-03-22 23:50:21 +0700

    fix: apply a patch for IOMMU issues in 6.19 kernels

--- nixos-configurations/common/kernel.nix
+++ nixos-configurations/common/kernel.nix
@@ -1,7 +1,17 @@
 { pkgs, lib, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxKernel.packageAliases.linux_latest;
+  boot.kernelPackages = pkgs.linuxKernel.packagesFor (pkgs.linuxKernel.kernels.linux_6_19.override (oldAttrs: {
+    kernelPatches = oldAttrs.kernelPatches ++ [
+      {
+        name = "fix-iommu";
+        patch = pkgs.fetchpatch2 {
+          url = "https://lore.kernel.org/linux-iommu/20260227080638.208693-1-lkml@antheas.dev/raw";
+          hash = "sha256-22mhO6KV2tr6Glhdlwq5yHPklsdtVR+NPP+KeU9fEQA=";
+        };
+      }
+    ];
+  }));
   specialisation.lts-linux.configuration = {
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
   };
