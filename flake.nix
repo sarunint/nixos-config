235a61714e7f334fc4dc537e8dc678090674337f
Sarun Intaralawan <sarunint@sarunint.com>
2026-06-25 22:57:06 +0700

    fix: use upstream Lanzaboote

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:sarunint/lanzaboote/no-bootspec";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts = {
