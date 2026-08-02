77d3f7f861cb02d7efdca2880f4c406344890ddc
Sarun Intaralawan <sarunint@sarunint.com>
2026-08-02 23:01:42 +0700

    feat: use patched Lanzaboote

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.1.0";
+      url = "github:sarunint/lanzaboote/xbootldr-efipatch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts = {
