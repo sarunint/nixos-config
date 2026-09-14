f3aaea1cc5c881dfec42dd64371d7ef1e4888287
Sarun Intaralawan <sarunint@sarunint.com>
2026-09-14 23:35:49 +0700

    feat: add Node.js v24 devshell

--- flake.nix
+++ flake.nix
@@ -218,6 +218,7 @@
           nodejs_18 = (import ./dev-shells/nodejs/18.nix { inherit pkgs; });
           nodejs_20 = (import ./dev-shells/nodejs/20.nix { inherit pkgs; });
           nodejs_22 = (import ./dev-shells/nodejs/22.nix { inherit pkgs; });
+          nodejs_24 = (import ./dev-shells/nodejs/24.nix { inherit pkgs; });
           minecraft = (import ./dev-shells/minecraft/minecraft.nix { inherit pkgs; });
         };
       };
